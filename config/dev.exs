0c12ea7b53134fe450856c177c8f94b11bc54128
Dave Lively <davelively14@gmail.com>
2015-12-27 07:28:22 -0500

    Fixed Postgres settings

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :haypoll, Haypoll.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "haypoll_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :haypoll, Haypoll.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "haypoll_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
